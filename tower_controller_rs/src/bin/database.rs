0c2891ecd2aa5eba9783ccfddf6e8f5b2b12595b
Joshua Lung <jo.kil.plays@gmail.com>
2023-01-11 18:45:54 +0100

    db lib documentation

--- tower_controller_rs/src/bin/database.rs
+++ tower_controller_rs/src/bin/database.rs
@@ -8,14 +8,26 @@ async fn main() {
     let project_id: String = std::env::var("PROJECT_ID").unwrap();
     let db = FirestoreDb::new(project_id).await.unwrap();
 
-    let t: Option<FirestoreTower> = db
+    // let t: Option<FirestoreTower> = db
+    //     .fluent()
+    //     .select()
+    //     .by_id_in("towers")
+    //     .obj()
+    //     .one("5aQQXeYkP0xfW3FJxjH0")
+    //     .await
+    //     .unwrap();
+
+    let t: Vec<FirestoreTest> = db
         .fluent()
         .select()
-        .by_id_in("towers")
+        .from("test")
         .obj()
-        .one("5aQQXeYkP0xfW3FJxjH0")
+        .query()
         .await
         .unwrap();
 
     println!("{:?}", t);
 }
+
+#[derive(Debug, Serialize, Deserialize)]
+struct FirestoreTest;
