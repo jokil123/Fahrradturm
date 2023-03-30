78cfd2aa175430c4a1e9c8da32e93b38b8b44d54
Pauhl <paul.hartmann@student.htldornbirn.at>
2023-03-30 20:26:12 +0200

    app aktuelle version

--- app/app/components/addUser.js
+++
@@ -1,8 +0,0 @@
-import { firestore } from './../firebase';
-import { collection, query, where, onSnapshot } from "firebase/firestore";
-
-
-export default function addUser(id, email) {
-    
-    const res = firestore.collection('user').doc(id).set({email: email});
-}
